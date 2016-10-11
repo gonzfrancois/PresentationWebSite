ae74ad6da2afd154e1cf84d16080d61874c81608
François <gonzalezf66@gmail.com>
2016-10-11 16:17:10 +0200

    Set Admin user as default user
    Delete Register action
    Set default user and default password from web.config if no
    administrator exist on application startup

--- src/PresentationWebSite.UI.WebMvc/Startup.cs
+++ src/PresentationWebSite.UI.WebMvc/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using System.Configuration;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using PresentationWebSite.UI.WebMvc.Models;
 
 [assembly: OwinStartupAttribute(typeof(PresentationWebSite.UI.WebMvc.Startup))]
 namespace PresentationWebSite.UI.WebMvc
@@ -9,6 +13,43 @@ namespace PresentationWebSite.UI.WebMvc
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        private void CreateRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            // In Startup iam creating first Administrator Role and creating a default Administrator User     
+            if (!roleManager.RoleExists("Administrator"))
+            {
+
+                // first we create Administrator rool    
+                var role = new IdentityRole {Name = "Administrator"};
+                roleManager.Create(role);
+
+                //HACK If UserName and Email aren't the same, we could'nt log in. This is a issue from Identity 2.0. See http://stackoverflow.com/a/24252833/2961285
+                var adminEmail = ConfigurationManager.AppSettings["AdminEmail"];
+                var user = new ApplicationUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail
+                };
+
+                var userPwd = ConfigurationManager.AppSettings["AdmineBasePwd"]; 
+
+                var chkUser = userManager.Create(user, userPwd);
+
+                //Add default User to Role Administrator    
+                if (chkUser.Succeeded)
+                {
+                    var result1 = userManager.AddToRole(user.Id, "Administrator");
+
+                }
+            }
         }
     }
 }
