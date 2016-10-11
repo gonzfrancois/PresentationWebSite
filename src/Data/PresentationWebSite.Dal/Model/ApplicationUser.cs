7b5643fb0ecf776f3dd5b6d22b65540e600113c5
François <gonzalezf66@gmail.com>
2016-10-11 18:15:50 +0200

    Add db identity

---
+++ src/Data/PresentationWebSite.Dal/Model/ApplicationUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationWebSite.Dal.Model
+{
+    public class ApplicationUser
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string FamilyName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+        public string ZipCode  { get; set; }
+
+        public ICollection<Text> DisplayWork { get; set; }
+        public ICollection<PresentationText> PresentationTexts { get; set; }
+
+    }
+
+    
+
+    public class PresentationText
+    {
+        public int Id { get; set; }
+        public ICollection<Text> Texts { get; set; }
+    }
+}
