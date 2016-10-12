ba49d044ab5f2af9ae71e11e73da8eefb6ca2fb9
François <gonzalezf66@gmail.com>
2016-10-12 19:02:49 +0200

    Delete unused tests

--- src/PresentationWebSite.UI.WebMvc.Tests/Controllers/HomeControllerTest.cs
+++ src/PresentationWebSite.UI.WebMvc.Tests/Controllers/HomeControllerTest.cs
@@ -9,33 +9,9 @@ using PresentationWebSite.UI.WebMvc.Controllers;
 
 namespace PresentationWebSite.UI.WebMvc.Tests.Controllers
 {
-    [TestClass]
+    
     public class HomeControllerTest
     {
-        [TestMethod]
-        public void Index()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Index() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
+        
     }
 }
